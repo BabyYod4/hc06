32829bb14030675eb6e364705559c3191778e49b
BabyYod4 <razzlenator@protonmail.com>
2020-07-27 13:05:35 +0200

    Merger from Bitbucket

---
+++ hc06.h
@@ -0,0 +1,30 @@
+#ifndef HC06_HPP
+#define HC06_HPP
+
+#include "../hal/hal.h"
+#include "../tools/data_types/byte_types.h" 
+
+#include "hc06_settings.h"
+
+typedef struct BluetoothDevice {
+    void (*init)(void);
+    void (*sendByte)(uint8_t);
+    void (*sendBuff)(uint8_t*, uint8_t)
+} BluetoothDevice;
+
+typedef struct HC06Interface{
+    gpio_t rxPin;
+    gpio_t txPin;
+    uint16_t baudRate;
+    mode_t uartMode;
+} HC06Interface;
+
+void hc60Init(void);
+void hc60SendByte(uint8_t msg);
+void hc60SendBuff(uint8_t* buff, uint8_t size);
+
+void CreateHc60Device(BluetoothDevice* self, HC06Interface* interface);
+
+void _hc60RxIRQ(void);
+
+#endif // HC06_HPP
